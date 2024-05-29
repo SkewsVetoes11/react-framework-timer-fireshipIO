02af16e4efa41464a21a82068436a57045b5dc5c
SkewsVetoes11 <skews_vetoes_0t@icloud.com>
2024-05-28 20:00:03 -0400

    almost done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./style.css";
+function CountDown({ hr, min, sec }) {
+  const [over, setOver] = useState(false);
+  const [pause, setPause] = useState(true);
+  const [[h, m, s], setTime] = useState([hr, min, sec]);
 
+  const tick = () => {
+    console.log("triggered");
+    if (pause || over) {
+      return;
+    } else if (s === 0) {
+      setTime([h, m - 1, 59]);
+    } else if (m === 0 && s === 0) {
+      setTime([h - 1, 59, 59]);
+    } else if (h === 0 && m === 0 && s === 0) {
+      setOver(true);
+    } else {
+      setTime([h, m, s - 1]);
+    }
+  };
+  useEffect(() => {
+    let ticker = setInterval(() => tick(), 1000);
+    return () => {
+      clearInterval(ticker);
+    };
+  });
+
+  const handlePause = () => {
+    setPause(!pause);
+  };
+
+  const handleReset = () => {
+    setTime([hr, min, sec]);
+    setPause(true);
+  };
+
+  let format = (num) => {
+    let output = num.toString().padStart(2, "0");
+    return output;
+  };
+
+  return (
+    <>
+      <h3 className="countDown">
+        {format(h)} : {format(m)} : {format(s)}
+      </h3>
+      <button onClick={handlePause}>{pause ? "Start" : "Pause"}</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CountDown hr={3} min={9} sec={10} />
+    </>
   );
 }
 
